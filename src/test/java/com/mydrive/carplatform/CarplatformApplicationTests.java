ffc4dbbc919d8c16a04d3740f81c426bae0c75bb
TheMuffinMan1320 <76186189+TheMuffinMan1320@users.noreply.github.com>
2026-08-11 18:32:25 -0500

    initial commit

---
+++ src/test/java/com/mydrive/carplatform/CarplatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mydrive.carplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarplatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
